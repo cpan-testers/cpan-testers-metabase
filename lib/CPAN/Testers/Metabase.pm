ddee7b5ce1e5e6bf7296a8407e78f287f0489c73
David Golden <dagolden@cpan.org>
2012-05-18 10:36:16 -0400

    fixed docs

--- lib/CPAN/Testers/Metabase.pm
+++ lib/CPAN/Testers/Metabase.pm
@@ -10,26 +10,27 @@ __END__
 
 =head1 SYNOPSIS
 
-  use CPAN::Testers::Metabase::AWS;
+  use CPAN::Testers::Metabase::Demo;
 
-  my $mb = CPAN::Testers::Metabase::AWS->new( %aws_args );
+  # defaults to directory on /tmp
+  my $mb = CPAN::Testers::Metabase::Demo->new;
 
-  my $librarian = $mb->public_librarian;
+  $mb->public_librarian->search( %search spec );
 
 =head1 DESCRIPTION
 
 The CPAN::Testers::Metabase namespace is intended to span a collection
-of subclasses that instantiate specific Metabase backend storage and indexing
+of classes that instantiate specific Metabase backend storage and indexing
 capabilities for a CPAN Testers style Metabase.
 
-Each subclass consumes the L<Metabase::Gateway> role and can be used by
+Each class consumes the L<Metabase::Gateway> role and can be used by
 the L<Metabase::Web> application as a data model.
 
 See specific classes for more detail:
 
 =for :list
 * [CPAN::Testers::Metabase::AWS] -- storage and indexing with Amazon Web Services
-* [CPAN::Testers::Metabase::MongoDB] -- MongoDB storage and indexing
+* [CPAN::Testers::Metabase::MongoDB] -- storage and indexing with MongoDB
 * [CPAN::Testers::Metabase::Demo] -- SQLite archive and flat-file index (for test/demo purposes only)
 
 =cut
